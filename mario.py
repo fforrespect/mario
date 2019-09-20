997284e561791cad7f4a07d999a4ce06a50216cf
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2019-09-20 14:55:07 +0400

    added comments to python file

--- mario.py
+++ mario.py
@@ -1,10 +1,16 @@
-
 while 1:
+
+    # exception handling to make sure that the input (var height) is always and integer
     try:
+
+        # input height and cast to an int
         height = int(input("Height: "))
+
+        # input needs to be within the constraints given
         if 0 <= height <= 23:
             break
     except:
         pass
 
+# prints the output, by using a for loop, and calculating the difference between the height and how many ' 's or '#'s have been printed
 print("\n".join([" "*(height-i) + "#"*i + "  " + "#"*i + " "*(height-i) for i in range(1, height+1)]))
