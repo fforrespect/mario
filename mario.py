ff92d81b463504a33695d1148d03fc72f976ab47
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2019-09-20 14:05:57 +0400

    finished mario.py, including input validation

--- mario.py
+++ mario.py
@@ -1,4 +1,10 @@
-height = int(input("Height: "))
 
-for i in range(height):
-    print(" "*(height-i) + "#"*i + "  " + "#"*i + " "*(height-i))
+while 1:
+    try:
+        height = int(input("Height: "))
+        if 0 <= height <= 23:
+            break
+    except:
+        pass
+
+print("\n".join([" "*(height-i) + "#"*i + "  " + "#"*i + " "*(height-i) for i in range(1, height+1)]))
