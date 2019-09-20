164e1d4d8fdd587095de859d4e58025aea6ac22b
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2019-09-20 14:24:46 +0400

    fully implemented c version. only input validation left to fix

--- mario.c
+++ mario.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-    
+  int height;
 
+  do {
+    printf("Height: ");
+    scanf("%i", &height);
+  } while(height < 0 || height > 23);
+
+  int i; int j; int n;
+
+  for (i=1; i<=height; i++) {
+    for (j=0, n=height-i; j<n; j++) {printf(" ");}
+    for (j=0; j<i; j++) {printf("#");}
+
+    printf("  ");
+
+    for (j=0; j<i; j++) {printf("#");}
+    for (j=0, n=height-i; j<n; j++) {printf(" ");}
+
+    printf("\n");
+
+  }
 }
